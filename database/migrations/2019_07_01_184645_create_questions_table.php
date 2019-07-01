024ba402ae6b04805d09c61942e4aa180c0d1b58
whiskpo0 <whiskypo082@gmail.com>
2019-07-01 14:49:25 -0500

    Factory and Seed

--- database/migrations/2019_07_01_184645_create_questions_table.php
+++ database/migrations/2019_07_01_184645_create_questions_table.php
@@ -18,7 +18,7 @@ class CreateQuestionsTable extends Migration
             $table->string('title');
             $table->string('slug');
             $table->text('body');
-            $table->integer('categoty_id')->unsigned();
+            $table->integer('category_id')->unsigned();
             $table->integer('user_id')->unsigned();            
             $table->timestamps();
         });
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 ##This is a real time single page forum app. 
 
-# We are using Laravel, Pusher, VueJS, Vuetify, JWT, and Markdown. 
+## We are using Laravel, Pusher, VueJS, Vuetify, JWT, and Markdown. 
